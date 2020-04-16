5e448b3875a8332d450c7bfd5c184726b5a875a6
Eric Saxby <sax@livinginthepast.org>
2020-04-16 10:16:19 -0700

    Add git-duet to oh-my-zsh

---
+++ ohmyzsh/plugins/git-duet/git-duet.plugin.zsh
@@ -0,0 +1,14 @@
+function git() {
+  if [[ "$1" == commit ]] ; then
+    shift
+    command git duet-commit "$@"
+  elif [[ "$1" == revert ]] ; then
+    shift
+    command git duet-revert "$@"
+  else
+    command git "$@"
+  fi
+}
+
+export GIT_DUET_ROTATE_AUTHOR=1
+export GIT_DUET_CO_AUTHORED_BY=1
