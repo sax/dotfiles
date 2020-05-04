70fcc0027b00722d6d64238ad978791efaedbc16
Eric Saxby <sax@livinginthepast.org>
2020-05-04 08:47:27 -0700

    Add more locations to clock

--- ohmyzsh/plugins/clock/clock.plugin.zsh
+++ ohmyzsh/plugins/clock/clock.plugin.zsh
@@ -1,4 +1,6 @@
 function clock() {
-  watch -t -n1 'echo "$(date +%a\ %b\ %e\ %Y%n%X\ %Z)\n$(date -u +%X\ %Z)" | figlet -k -c -w$(tput cols)'
+  watch -t -n1 \
+    'echo "$(date +%a\ %b\ %e\ %Y%n%Z\ %X)\n$(date -u +%Z\ %X)\nLondon $(TZ=Europe/London date +%X)\nBerlin $(TZ=Europe/Berlin date +%X)\nJakarta $(TZ=Asia/Jakarta date +%X)" \
+       | figlet -k -c -w$(tput cols)'
 }
 
