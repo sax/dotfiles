31a50018f4084ce2758e097ead4bcb5c5a5babef
Eric Saxby <sax@livinginthepast.org>
2020-11-03 11:39:59 -0800

    Swap out clock locations

--- ohmyzsh/plugins/clock/clock.plugin.zsh
+++ ohmyzsh/plugins/clock/clock.plugin.zsh
@@ -1,6 +1,6 @@
 function clock() {
   watch -t -n1 \
-    'echo "$(date +%a\ %b\ %e\ %Y%n%Z\ %X)\n$(date -u +%Z\ %X)\nLondon $(TZ=Europe/London date +%X)\nBerlin $(TZ=Europe/Berlin date +%X)\nJakarta $(TZ=Asia/Jakarta date +%X)" \
+    'echo "$(date +%a\ %b\ %e\ %Y%n%Z\ %X)\n$(date -u +%Z\ %X)\nNY $(TZ=America/New_York date +%X)\nLondon $(TZ=Europe/London date +%X)\nBerlin $(TZ=Europe/Berlin date +%X)\nJakarta $(TZ=Asia/Jakarta date +%X)" \
        | figlet -k -c -w$(tput cols)'
 }
 
