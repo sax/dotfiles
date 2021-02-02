6d9aa0128d82319b3e6639864947c9f47655d9a6
Eric Saxby <sax@livinginthepast.org>
2021-02-02 11:53:46 -0800

    Add gcloud

--- Brewfile
+++ Brewfile
@@ -8,10 +8,13 @@ brew 'figlet'
 # brew 'hub'
 brew 'neovim'
 # brew 'nvm'
+brew 'python@3.9'
 brew 'readline'
 brew 'the_silver_searcher'
 brew 'watch'
 brew 'zlib'
+
+cask 'google-cloud-sdk'
 
 # cask 'firefox'
 # cask 'google-chrome'
---
+++ ohmyzsh/gcloud.zsh
@@ -0,0 +1,3 @@
+export CLOUDSDK_PYTHON="$(brew --prefix)/opt/python@3.9/libexec/bin/python"
+source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc
+source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
