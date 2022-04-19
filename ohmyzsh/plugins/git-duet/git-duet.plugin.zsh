69ed6f554e30d3e633fdba37320ee2fd26d395d6
Eric Saxby <sax@livinginthepast.org>
2022-04-19 11:59:56 -0700

    Comment git duet zsh plugin

--- ohmyzsh/plugins/git-duet/git-duet.plugin.zsh
+++ ohmyzsh/plugins/git-duet/git-duet.plugin.zsh
@@ -1,15 +1,16 @@
-function git() {
-  if [[ "$1" == commit ]] ; then
-    shift
-    command git duet-commit "$@"
-  elif [[ "$1" == revert ]] ; then
-    shift
-    command git duet-revert "$@"
-  else
-    command git "$@"
-  fi
-}
+# function git() {
+#   if [[ "$1" == commit ]] ; then
+#     shift
+#     command git duet-commit "$@"
+#   elif [[ "$1" == revert ]] ; then
+#     shift
+#     command git duet-revert "$@"
+#   else
+#     command git "$@"
+#   fi
+# }
 
-export GIT_DUET_ROTATE_AUTHOR=1
-export GIT_DUET_CO_AUTHORED_BY=1
-export GIT_DUET_SET_GIT_USER_CONFIG=1
+# export GIT_DUET_ROTATE_AUTHOR=1
+# export GIT_DUET_CO_AUTHORED_BY=1
+# export GIT_DUET_SET_GIT_USER_CONFIG=1
+
