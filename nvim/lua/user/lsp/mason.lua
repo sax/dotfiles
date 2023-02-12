a5ed576aaa8aad55bf8f1ae0b07cc15f334d0b5b
Eric Saxby <sax@livinginthepast.org>
2023-02-12 11:06:17 -0800

    Remove automatically-installed language servers

--- nvim/lua/user/lsp/mason.lua
+++ nvim/lua/user/lsp/mason.lua
@@ -1,29 +1,22 @@
-local servers = {
-    "elixirls",
-    "erlangls",
-    "jsonls",
-    "rust_analyzer",
-    "sumneko_lua",
-    "tsserver",
-  }
+local servers = {}
 
 local settings = {
-  ui = {
-    border = "none",
-    icons = {
-      package_installed = "◍",
-      package_pending = "◍",
-      package_uninstalled = "◍",
+    ui = {
+        border = "none",
+        icons = {
+            package_installed = "◍",
+            package_pending = "◍",
+            package_uninstalled = "◍",
+        },
     },
-  },
-  log_level = vim.log.levels.INFO,
-  max_concurrent_installers = 4,
+    log_level = vim.log.levels.INFO,
+    max_concurrent_installers = 4,
 }
 
 require("mason").setup(settings)
 require("mason-lspconfig").setup({
-  ensure_installed = servers,
-  automatic_installation = true,
+    ensure_installed = servers,
+    automatic_installation = true,
 })
 
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
@@ -36,8 +29,8 @@ local opts = {}
 
 for _, server in pairs(servers) do
   opts = {
-    on_attach = require("user.lsp.handlers").on_attach,
-    capabilities = require("user.lsp.handlers").capabilities,
+      on_attach = require("user.lsp.handlers").on_attach,
+      capabilities = require("user.lsp.handlers").capabilities,
   }
 
   server = vim.split(server, "@")[1]
