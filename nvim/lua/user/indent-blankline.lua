c77bdf2f6c95836aa90b942ff0a4321af9dd85b5
Eric Saxby <sax@livinginthepast.org>
2023-10-06 14:10:20 -0700

    Desaturate nvim indentation colors

--- nvim/lua/user/indent-blankline.lua
+++ nvim/lua/user/indent-blankline.lua
@@ -21,12 +21,12 @@ local highlight = {
 
 hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
   vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#93aabd" })
-  vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-  vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-  vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-  vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-  vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-  vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+  vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#769da2" })
+  vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#9cad8f" })
+  vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#b19b86" })
+  vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#bd8f92" })
+  vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#b596bf" })
+  vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#c5b69b" })
 end)
 
 indent_blankline.setup {
