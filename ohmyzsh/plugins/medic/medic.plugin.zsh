d00599cc0d205998f8cbdbf12fef20acd4ba6ca2
Eric Saxby <sax@livinginthepast.org>
2024-03-17 13:27:39 -0700

    omz: medic plugin prefers .config/medic.toml to bin scripts

--- ohmyzsh/plugins/medic/medic.plugin.zsh
+++ ohmyzsh/plugins/medic/medic.plugin.zsh
@@ -1,10 +1,10 @@
 function doc() {
-  if [[ -f bin/dev/doctor ]]; then
-    echo "== \e[37;1mRunning bin/dev/doctor\e[0m"
-    bin/dev/doctor
-  elif [[ -f .config/medic.toml ]]; then
+  if [[ -f .config/medic.toml ]]; then
     echo "== \e[37;1mRunning medic doc\e[0m"
     medic doc
+  elif [[ -f bin/dev/doctor ]]; then
+    echo "== \e[37;1mRunning bin/dev/doctor\e[0m"
+    bin/dev/doctor
   else
     echo "Neither bin/dev/doctor nor .config/medic.toml exist" >&2
     return 1
@@ -12,12 +12,12 @@ function doc() {
 }
 
 function shipit() {
-  if [[ -f bin/dev/shipit ]]; then
-    echo "== \e[37;1mRunning bin/dev/shipit\e[0m"
-    bin/dev/shipit
-  elif [[ -f .config/medic.toml ]]; then
+  if [[ -f .config/medic.toml ]]; then
     echo "== \e[37;1mRunning medic ship\e[0m"
     medic ship
+  elif [[ -f bin/dev/shipit ]]; then
+    echo "== \e[37;1mRunning bin/dev/shipit\e[0m"
+    bin/dev/shipit
   else
     echo "Neither bin/dev/shipit nor .config/medic.toml exist" >&2
     return 1
@@ -25,15 +25,15 @@ function shipit() {
 }
 
 function t() {
-  if [[ -f bin/dev/test ]]; then
+  if [[ -f .config/medic.toml ]]; then
+    echo "== \e[37;1mRunning medic test\e[0m"
+    medic test
+  elif [[ -f bin/dev/test ]]; then
     echo "== \e[37;1mRunning bin/dev/test\e[0m"
     bin/dev/test
   elif [[ -f bin/dev/run-tests ]]; then
     echo "== \e[37;1mRunning bin/dev/run-tests\e[0m"
     bin/dev/run-tests
-  elif [[ -f .config/medic.toml ]]; then
-    echo "== \e[37;1mRunning medic test\e[0m"
-    medic test
   else
     echo "Neither bin/dev/{run-tests,test} nor .config/medic.toml exist" >&2
     return 1
@@ -41,12 +41,12 @@ function t() {
 }
 
 function up() {
-  if [[ -f bin/dev/update ]]; then
-    echo "== \e[37;1mRunning bin/dev/update\e[0m"
-    bin/dev/update
-  elif [[ -f .config/medic.toml ]]; then
+  if [[ -f .config/medic.toml ]]; then
     echo "== \e[37;1mRunning medic up\e[0m"
     medic up
+  elif [[ -f bin/dev/update ]]; then
+    echo "== \e[37;1mRunning bin/dev/update\e[0m"
+    bin/dev/update
   else
     echo "Neither bin/dev/update nor .config/medic.toml exist" >&2
     return 1
