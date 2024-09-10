2c421d16bfee97acc5baa2ef2f112e8b783d7e05
Eric Saxby <sax@livinginthepast.org>
2024-09-10 09:14:35 -0700

    omz: medic plugin passes args to commands

--- ohmyzsh/plugins/medic/medic.plugin.zsh
+++ ohmyzsh/plugins/medic/medic.plugin.zsh
@@ -1,13 +1,13 @@
 function doc() {
   if [[ -f .config/medic.toml ]]; then
     echo "== \e[37;1mRunning medic doc\e[0m"
-    medic doc
+    medic doc $@
   elif [[ -n "${MEDIC_CONFIG}" ]]; then
     echo "== \e[37;1mRunning medic doc\e[0m"
-    medic doc
+    medic doc $@
   elif [[ -f bin/dev/doctor ]]; then
     echo "== \e[37;1mRunning bin/dev/doctor\e[0m"
-    bin/dev/doctor
+    bin/dev/doctor $@
   else
     echo "Neither bin/dev/doctor nor .config/medic.toml exist" >&2
     return 1
@@ -17,13 +17,13 @@ function doc() {
 function shipit() {
   if [[ -f .config/medic.toml ]]; then
     echo "== \e[37;1mRunning medic ship\e[0m"
-    medic ship
+    medic ship $@
   elif [[ -n "${MEDIC_CONFIG}" ]]; then
     echo "== \e[37;1mRunning medic ship\e[0m"
-    medic ship
+    medic ship $@
   elif [[ -f bin/dev/shipit ]]; then
     echo "== \e[37;1mRunning bin/dev/shipit\e[0m"
-    bin/dev/shipit
+    bin/dev/shipit $@
   else
     echo "Neither bin/dev/shipit nor .config/medic.toml exist" >&2
     return 1
@@ -33,16 +33,16 @@ function shipit() {
 function t() {
   if [[ -f .config/medic.toml ]]; then
     echo "== \e[37;1mRunning medic test\e[0m"
-    medic test
+    medic test $@
   elif [[ -n "${MEDIC_CONFIG}" ]]; then
     echo "== \e[37;1mRunning medic test\e[0m"
-    medic test
+    medic test $@
   elif [[ -f bin/dev/test ]]; then
     echo "== \e[37;1mRunning bin/dev/test\e[0m"
-    bin/dev/test
+    bin/dev/test $@
   elif [[ -f bin/dev/run-tests ]]; then
     echo "== \e[37;1mRunning bin/dev/run-tests\e[0m"
-    bin/dev/run-tests
+    bin/dev/run-tests $@
   else
     echo "Neither bin/dev/{run-tests,test} nor .config/medic.toml exist" >&2
     return 1
@@ -52,13 +52,13 @@ function t() {
 function up() {
   if [[ -f .config/medic.toml ]]; then
     echo "== \e[37;1mRunning medic up\e[0m"
-    medic up
+    medic up $@
   elif [[ -n "${MEDIC_CONFIG}" ]]; then
     echo "== \e[37;1mRunning medic up\e[0m"
-    medic up
+    medic up $@
   elif [[ -f bin/dev/update ]]; then
     echo "== \e[37;1mRunning bin/dev/update\e[0m"
-    bin/dev/update
+    bin/dev/update $@
   else
     echo "Neither bin/dev/update nor .config/medic.toml exist" >&2
     return 1
