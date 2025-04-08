a633b5b0417ffc049df298067c378d116fc3c801
Eric Saxby <sax@livinginthepast.org>
2025-04-08 15:08:33 -0700

    Remove archived nvim null-ls plugin

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -110,7 +110,7 @@ return packer.startup(function(use)
   use "williamboman/mason.nvim" -- simple to use language server installer
   use "williamboman/mason-lspconfig.nvim"
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
-  use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+  -- use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
   -- use "RRethy/vim-illuminate"
 
   -- Telescope
